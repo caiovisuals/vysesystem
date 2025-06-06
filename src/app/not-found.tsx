c46f65f2b01c17c74b236cd9123a63f4b6a780b6
caiothevisual <68815383+caiovisuals@users.noreply.github.com>
2025-06-05 22:26:56 -0300

    primeirin

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "Erro 404 - VyseSystem",
+	description: "Erro 404",
+}
+
+export default function NotFound() {
+    return (
+        <div></div>
+    )
+}
